4fa4c14d27469412a9c87a1dc533b7a8ae9a772b
Enzo Lima <129903213+thenzolima@users.noreply.github.com>
2024-11-26 09:43:35 -0300

    responsividade

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Ajustes from "./pages/Ajustes.jsx";
